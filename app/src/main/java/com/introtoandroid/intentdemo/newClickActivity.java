9e823a4edd87e3fa39d75ecf1216556def25b335
jacobmattox <jacob.mattox1017@gmail.com>
2019-02-06 18:45:26 -0500

    buttons and activity set up

---
+++ app/src/main/java/com/introtoandroid/intentdemo/newClickActivity.java
@@ -0,0 +1,13 @@
+package com.introtoandroid.intentdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class newClickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_click);
+    }
+}
